a5c4367e47572e52179bb3fb8f25d93f725cfa33
Asif Mahmud <asiffyy@gmail.com>
2021-07-14 01:13:45 +0600

    working on home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,10 +3,29 @@ import style from "./Home.module.css";
 
 import NavBar from "../NavBar/NavBar";
 
+//assets import
+import heroImg from "../../assets/heroImage.png";
+
 const Home = () => {
   return (
     <div className={style.home}>
       <NavBar />
+      <div className={style.heroSection}>
+        <div className="container">
+          <div className={style.heroSectionContent}>
+            <div className={style.tagLineBox}>
+              <h1 className={style.tagLineWords}>CLASSIC</h1>
+              <h1 className={style.tagLineWords}>WITH</h1>
+              <h1 className={style.tagLineWordHighlight}>MODERN</h1>
+              <h1 className={style.tagLineWords}>TWIST</h1>
+              <p>Hoodies, jackets & sweaters</p>
+            </div>
+            <div className={style.heroImage}>
+              <img src={heroImg}></img>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
