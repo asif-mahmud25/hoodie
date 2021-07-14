07fd2725d5250acb2bf8630d7e8a46d1b7e639d5
Asif Mahmud <asiffyy@gmail.com>
2021-07-15 01:11:19 +0600

    making sidebar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./SideBar.module.css";
 
 //assets imports
 import menuBars from "../../assets/menu-bars.svg";
 
 const SideBar = () => {
+  const [sideBarOpen, setSidebarOpen] = useState(false);
+
+  //open sidebar
+  const openSidebar = () => {
+    setSidebarOpen(true);
+  };
+
+  //close sidebar
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
   return (
     <div className={style.sideBar}>
       <div className={style.sideBarHeader}>
         <div className="container">
           <div className={style.sideBarHeaderContent}>
             <h1>hoodie.</h1>
-            <img src={menuBars} alt="Menu" />
+            <img src={menuBars} alt="Menu" onClick={openSidebar} />
           </div>
         </div>
       </div>
-      <div className={style.sideBarBox}></div>
+      <div
+        className={
+          sideBarOpen
+            ? style.sideBarBoxBackdrop
+            : style.sideBarBoxBackdropHidden
+        }
+        onClick={closeSidebar}
+      ></div>
+      <div
+        className={sideBarOpen ? style.sideBarBoxOpen : style.sideBarBoxClosed}
+      >
+        hoodie
+      </div>
     </div>
   );
 };
