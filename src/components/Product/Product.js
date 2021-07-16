2eb39359d1609f0b47546af5bb0d521f4be62b97
Asif Mahmud <asiffyy@gmail.com>
2021-07-17 00:42:57 +0600

    fetching product from db

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,23 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./Product.module.css";
+import { useParams, useHistory } from "react-router-dom";
+import { db } from "../../firebase";
 
 const Product = () => {
+  //to get the product id
+  const params = useParams();
+
+  //for navigation
+  const history = useHistory();
+
+  //product state
+  const [product, setProduct] = useState({
+    id: "",
+    imgUrl: "",
+    name: "",
+    description: "",
+    price: "",
+  });
+
+  let productHolder = {};
+
+  useEffect(() => {
+    db.collection("products")
+      .doc(`${params.id}`)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          //for test
+          console.log(doc.data());
+
+          productHolder = {
+            id: doc.data().id,
+            imgUrl: doc.data().imgL,
+            name: doc.data().name,
+            description: doc.data().description,
+            price: doc.data().price,
+          };
+
+          setProduct(productHolder);
+        } else {
+          //if there not such product go to home
+          history.push("/");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    // eslint-disable-next-line
+  }, [params.id]);
+
+  //for test
+  console.log(product);
   return (
     <div className={style.product}>
       <div className="container">
         <div className={style.productContent}>
-          <img
-            src="https://firebasestorage.googleapis.com/v0/b/hoodie-94315.appspot.com/o/p6-large.png?alt=media&token=54b5c318-afb9-4d3e-a4ac-390c79506e53"
-            alt="product-image"
-          />
+          <img src={product.imgUrl} alt="product" />
 
           <div className={style.productDetails}>
-            <h2>Classic leather jacket</h2>
-            <p>
-              Classic style men's black jacket. Made from genuine soft cow
-              leather. Best for bikers who rides with style
-            </p>
-            <h1>$86.60</h1>
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+            <h1>{product.price}</h1>
             <div className={style.sizeButtonsContainer}>
               <button className={style.sizeButton}>S</button>
               <button className={style.sizeButton}>M</button>
