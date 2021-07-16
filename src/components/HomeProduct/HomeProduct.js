df6049b858b1425f906965e420ecee78d2df8ac2
Asif Mahmud <asiffyy@gmail.com>
2021-07-16 19:09:15 +0600

    added img placeholder

--- src/components/HomeProduct/HomeProduct.js
+++ src/components/HomeProduct/HomeProduct.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./HomeProduct.module.css";
 
+//assets import
+import placeholderImg from "../../assets/placeholder-img.svg";
+
 const HomeProduct = (props) => {
+  //loading state for images
+  const [loading, setLoading] = useState(true);
+
+  //change state on image load
+  const showImg = () => {
+    setLoading(false);
+  };
+
   return (
     <div className={style.homeProduct}>
-      <img src={props.imageUrl} alt="" />
+      <img
+        className={loading ? style.show : style.hide}
+        src={placeholderImg}
+        alt=""
+      />
+      <img
+        className={loading ? style.hide : style.show}
+        src={props.imageUrl}
+        alt=""
+        onLoad={showImg}
+      />
       <div className={style.productInfo}>
         <h3>{props.name}</h3>
         <p>{props.description}</p>
