7baeece3dc6a737cfc627889cefc363421ca2244
Asif Mahmud <asiffyy@gmail.com>
2021-07-18 20:36:53 +0600

    added cart item placeholder img

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./CartItem.module.css";
 
+//assets import
+import placeholderImg from "../../assets/placeholder-img-sm.svg";
+
 const CartItem = (props) => {
+  //image load state
+  const [imgLoading, setImgLoading] = useState(true);
   return (
     <div className={style.cartItem}>
       <div className={style.cartItemContent}>
-        <img src={props.imgUrl} alt="" />
+        <img
+          className={imgLoading ? style.hide : style.show}
+          src={props.imgUrl}
+          alt=""
+          onLoad={() => {
+            setImgLoading(false);
+          }}
+        />
+        <img
+          className={imgLoading ? style.show : style.hide}
+          src={placeholderImg}
+          alt=""
+        />
         <div className={style.cartItemDetailsBox}>
           <div className={style.cartItemInfo}>
             <h4>{props.name}</h4>
