402586eac45f99cc750ce799533b8e4bdbe6dbf8
Asif Mahmud <asiffyy@gmail.com>
2021-07-30 20:48:01 +0600

    favorite item style

--- src/components/FavoriteItem/FavoriteItem.js
+++ src/components/FavoriteItem/FavoriteItem.js
@@ -9,29 +9,31 @@ const FavoriteItem = () => {
   const [imgLoading, setImgLoading] = useState(true);
   return (
     <div className={style.favoriteItem}>
-      <div className={style.favoriteItemContent}>
-        <img
-          src={placeholderImg}
-          alt="product"
-          className={imgLoading ? style.show : style.hide}
-        />
-        <img
-          src="https://firebasestorage.googleapis.com/v0/b/hoodie-94315.appspot.com/o/p2-small.png?alt=media&token=9ad1c02b-62f9-4f00-93c1-62946ad8dc2d"
-          alt="product"
-          className={imgLoading ? style.hide : style.show}
-          onLoad={() => {
-            setImgLoading(false);
-          }}
-        />
-        <div className={style.favoriteItemDetails}>
-          <div className={style.nameAndPrice}>
-            <p>Classic leather jacket</p>
-            <p>$86.60</p>
-          </div>
-          <p className={style.itemSize}>Size: M</p>
-          <div className={style.favoriteItemAction}>
-            <button>Add To Cart</button>
-            <p>Remove</p>
+      <div className={style.favoriteItemBox}>
+        <div className={style.favoriteItemContent}>
+          <img
+            src={placeholderImg}
+            alt="product"
+            className={imgLoading ? style.show : style.hide}
+          />
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/hoodie-94315.appspot.com/o/p2-small.png?alt=media&token=9ad1c02b-62f9-4f00-93c1-62946ad8dc2d"
+            alt="product"
+            className={imgLoading ? style.hide : style.show}
+            onLoad={() => {
+              setImgLoading(false);
+            }}
+          />
+          <div className={style.favoriteItemDetails}>
+            <div className={style.nameAndPrice}>
+              <p>Classic leather jacket</p>
+              <p>$86.60</p>
+            </div>
+            <p className={style.itemSize}>Size: M</p>
+            <div className={style.favoriteItemAction}>
+              <button>Add To Cart</button>
+              <p>Remove</p>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,11 +1,18 @@
 import React from "react";
 import style from "./Favorites.module.css";
 
+//components import
+import FavoriteItem from "../FavoriteItem/FavoriteItem";
+
 const Favorites = () => {
   return (
     <div className={style.favorites}>
       <div className="container">
         <h1 className={style.pageHeader}>YOUR FAVORITES</h1>
+        <FavoriteItem />
+        <FavoriteItem />
+        <FavoriteItem />
+        <FavoriteItem />
       </div>
     </div>
   );
